1fb48112aaecf3e776682e9209a3c0d715723409
Collette <cece554@gmail.com>
2025-06-26 09:54:32 -0500

    Update chat entry componet and pass props in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import './App.css';
+import ChatEntry from './components/ChatEntry';
+import messagesData from './data/messages.json';
 
 const App = () => {
+  const firstMessage = messagesData[0];
+
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>Chat Log</h1>
       </header>
       <main>
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
+        <ChatEntry 
+          sender={firstMessage.sender}
+          body={firstMessage.body}
+          timeStamp={firstMessage.timeStamp}
+        />
       </main>
     </div>
   );
